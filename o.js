a30bf6804f6f848da1f8d45308fc7a6b5b658dcb
MANVENDRA SINGH <Manvendra Singh Songara>
2021-04-01 19:18:12 +0530

    using property

--- o.js
+++ o.js
@@ -3,8 +3,8 @@ class A {
     //1.property
      name="";
     //2.constructor
-    constructor(a) {
-    this.name=a;//create internal object of the class
+    constructor(n) {
+    this.name=n;//create internal object of the class
     }
     //3.method
     myFunction() {
@@ -13,8 +13,24 @@ class A {
     }
 }
 //class end
+
+//class create
+class B extends A {
+    //1.property
+    
+    //2.constructor
+    constructor(n) {
+    super(n);
+    }
+    //3.method
+    myFunction1() {
+        console.log(`hello ${this.name} good morning`);
+
+    }
+}
+//class end
 //create an external object of the class
 //let object=new ClassName();
-let obj =new A("antim");
-obj.myFunction();
+let obj =new B("antim");
+obj.myFunction1();
 
