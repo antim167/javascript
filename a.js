66e1ac0ea204560e65d241e7d1605024e74dd6ef
MANVENDRA SINGH <Manvendra Singh Songara>
2021-04-02 16:46:46 +0530

    object

--- a.js
+++ a.js
@@ -1,6 +1,37 @@
-var a,b,c;
+/*creat object data type
+{
+    1.property
 
-a=20;
-b=30;
-c=a+b;
-console.log("addition is:-"+c);
+    2.methods
+}
+*/
+
+var a={
+    //1.property
+    fname:"antim",
+    'lname':"songara",
+    mob:961767,
+    "email":"antim34gmail.com", 
+
+    //2.methods  
+    full_name:function(){
+        return this.fname + " " +this.lname;
+    },
+    get_city:function(){
+      return("neemuch");
+    }
+};
+/*for(var b in a){
+    console.log( `${b}=${a[b]}`);
+}*/
+console.log(a.fname);
+console.log(a.lname);
+console.log(a.email);
+console.log(a.mob);
+console.log(a.full_name());
+console.log(a.get_city())
+
+//use loop
+for(var b in a){
+    console.log( `${b}=${a[b]}`);
+}
